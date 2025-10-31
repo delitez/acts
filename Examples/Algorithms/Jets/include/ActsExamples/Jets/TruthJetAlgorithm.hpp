081a9ac9ab73b777b0eface14634c819b854fffb
Doga Elitez <108287101+delitez@users.noreply.github.com>
2025-10-31 15:42:33 +0100

    Add labeling variables to config

--- Examples/Algorithms/Jets/include/ActsExamples/Jets/TruthJetAlgorithm.hpp
+++ Examples/Algorithms/Jets/include/ActsExamples/Jets/TruthJetAlgorithm.hpp
@@ -22,6 +22,10 @@ namespace fastjet {
 class PseudoJet;
 }
 
+namespace HepMC3 {
+  class GenEvent;
+}
+
 namespace ActsExamples {
 struct AlgorithmContext;
 
@@ -36,11 +40,31 @@ class TruthJetAlgorithm final : public IAlgorithm {
     /// Output jets collection.
     std::string outputJets;
     /// Minimum jet pT.
-    double jetPtMin;
+    double jetPtMin = 20 * Acts::UnitConstants::GeV;
+    /// Jet eta range
+    std::pair<std::optional<double>, std::optional<double>> jetEtaRange = {
+        std::nullopt, std::nullopt};
+    /// Jet clustering radius
+    double jetClusteringRadius = 0.4;
+    /// Only cluster HS particles
+    bool clusterHSParticlesOnly = true;
+    /// input HepMC3 event
+    std::optional<std::string> inputHepMC3Event;
+    /// Do jet labeling
+    bool doJetLabeling = true;
+    /// Delta R for labeling
+    double jetLabelingDeltaR = 0.4;
+    /// Minimum hadron pT for labeling
+    double jetLabelingHadronPtMin = 5 * Acts::UnitConstants::GeV;
+    /// Only label HS hadrons
+    bool jetLabelingHSHadronsOnly = true;
+    /// Output for debugging
+    bool debugCsvOutput = false;
   };
 
   TruthJetAlgorithm(const Config& cfg, Acts::Logging::Level lvl);
 
+  ProcessCode initialize() override;
   ProcessCode execute(const AlgorithmContext& ctx) const override;
   ProcessCode finalize() override;
 
@@ -50,7 +74,16 @@ class TruthJetAlgorithm final : public IAlgorithm {
   Config m_cfg;
   ReadDataHandle<SimParticleContainer> m_inputTruthParticles{
       this, "inputTruthParticles"};
+  ReadDataHandle<std::shared_ptr<HepMC3::GenEvent>> m_inputHepMC3Event{
+      this, "inputHepMC3Event"};
   WriteDataHandle<TruthJetContainer> m_outputJets{this, "outputJets"};
+
+  /// Statistics for jets
+  mutable std::atomic<std::size_t> m_numJets = 0;
+  mutable std::atomic<std::size_t> m_numJetsAfterOverlapRemoval = 0;
+  mutable std::atomic<std::size_t> m_numLightJets = 0;
+  mutable std::atomic<std::size_t> m_numCJets = 0;
+  mutable std::atomic<std::size_t> m_numBJets = 0;
 };
 
 }  // namespace ActsExamples
