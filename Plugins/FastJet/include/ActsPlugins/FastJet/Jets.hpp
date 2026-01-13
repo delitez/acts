fec7f0647911256578f14160999d6fee7c696e9e
Doga Elitez <108287101+delitez@users.noreply.github.com>
2026-01-13 13:40:59 +0100

    Revert "Changes in Jets.hpp"
    This reverts commit eeaa7ebbae52e42d0c4c47efbeeac1b42b5f3292.

--- Plugins/FastJet/include/ActsPlugins/FastJet/Jets.hpp
+++ Plugins/FastJet/include/ActsPlugins/FastJet/Jets.hpp
@@ -105,8 +105,10 @@ class TruthJet : public Jet {
   }
 
   /// @brief Set the tracks associated to this truth jet
-  void setAssociatedTracks(std::vector<std::int32_t>& associatedTrackIndices) {
-    m_associatedTrackIndices = associatedTrackIndices;
+  void setAssociatedTracks(
+      const std::vector<typename TrackContainer::TrackProxy>&
+          associatedTracks) {
+    m_associatedTracks = associatedTracks;
   }
 
   /// @brief Get the tracks associated to this truth jet
@@ -122,8 +124,6 @@ class TruthJet : public Jet {
   std::vector<int> m_constituentIndices;
   /// @brief The tracks associated to this truth jet
   std::vector<typename TrackContainer::TrackProxy> m_associatedTracks;
-  /// @brief The indices of the associated tracks in the input track collection
-  std::vector<std::int32_t> m_associatedTrackIndices;
 };
 
 template <typename TrackContainer>
