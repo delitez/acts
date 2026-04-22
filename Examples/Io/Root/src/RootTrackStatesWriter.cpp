32a68e9fd39fbb153a67f0706a31f37ef78cabea
Doga Elitez <108287101+delitez@users.noreply.github.com>
2026-04-22 14:40:27 +0200

    add git pre commit fix

--- Examples/Io/Root/src/RootTrackStatesWriter.cpp
+++ Examples/Io/Root/src/RootTrackStatesWriter.cpp
@@ -409,7 +409,9 @@ ProcessCode RootTrackStatesWriter::writeT(const AlgorithmContext& ctx,
       } else {
         // get the truth hits corresponding to this trackState
         // Use average truth in the case of multiple contributing sim hits
-        if (!state.hasUncalibratedSourceLink() || state.getUncalibratedSourceLink().template getPtr<IndexSourceLink>() == nullptr) {
+        if (!state.hasUncalibratedSourceLink() ||
+            state.getUncalibratedSourceLink()
+                    .template getPtr<IndexSourceLink>() == nullptr) {
           continue;
         }
         const auto sl =
